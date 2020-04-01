093528db6b93eee2f8e4de4865a42bda7ec1a776
Makenna Smutz <makennasmutz@gmail.com>
2020-04-01 12:44:08 +0200

    fix section anchor styling

--- src/components/organisims/section.jsx
+++ src/components/organisims/section.jsx
@@ -4,18 +4,22 @@ import { Box, Heading, Link } from "@chakra-ui/core"
 export const Section = ({ children, heading, anchor }) => (
   <Box as="section" maxW="1200px" mx="auto" py={12}>
     {heading ? (
-      <Link id={anchor}>
-        <Heading
-          as="h2"
-          color="gray.900"
-          fontSize="3xl"
-          fontWeight={900}
-          textAlign="center"
-          mb={12}
-        >
-          {heading}
-        </Heading>
-      </Link>
+      <Heading
+        as="h2"
+        color="gray.900"
+        fontSize="3xl"
+        fontWeight={900}
+        textAlign="center"
+        mb={12}
+      >
+        {anchor ? (
+          <Link id={anchor} _hover={{ textDecoration: "none", cursor: "auto" }}>
+            {heading}
+          </Link>
+        ) : (
+          heading
+        )}
+      </Heading>
     ) : null}
     {children}
   </Box>
