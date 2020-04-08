6b3d1c24af65600cc4dafcd6d98d643640bf4d9c
Makenna Smutz <makennasmutz@gmail.com>
2020-04-08 10:28:12 +0200

    Add the content calendar to the posts folder. Refactor to not build markdown files

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,7 +42,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
-        extensions: [`.mdx`, `.md`],
+        extensions: [`.mdx`],
       },
     },
     `gatsby-transformer-sharp`,
