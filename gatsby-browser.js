8c2e45929a33140ca361baa4ae5eb07d57366e7c
Makenna Smutz <makennasmutz@gmail.com>
2020-05-14 18:06:25 +0200

    add prismjs and with it support for haskell, scheme, and latex syntax highlighting

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,11 @@
 import React from "react"
 import { ThemeProvider, CSSReset } from "@chakra-ui/core"
 import customTheme from "./theme/theme"
+import Prism from "prism-react-renderer/prism"
+;(typeof global !== "undefined" ? global : window).Prism = Prism
+require("prismjs/components/prism-haskell")
+require("prismjs/components/prism-scheme")
+require("prismjs/components/prism-latex")
 
 export const wrapRootElement = ({ element }) => {
   return (
