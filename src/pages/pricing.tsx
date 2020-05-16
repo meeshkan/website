fad0372af3c69d7604ce3150716d7c4c6f56ee57
Makenna Smutz <makennasmutz@gmail.com>
2020-05-16 13:03:09 +0200

    resolves MEM-598 to convert the singleSection component to tsx and initializes the pricing page

---
+++ src/pages/pricing.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { Box } from "@chakra-ui/core"
+import { SingleSection } from "../components/organisms/singleSection"
+import Layout from "../components/templates/layout"
+
+const PricingPage = () => {
+  return (
+    <Layout>
+      <SingleSection heading="Meeshkan Pricing">
+        <Box>yo</Box>
+      </SingleSection>
+    </Layout>
+  )
+}
+
+export default PricingPage
