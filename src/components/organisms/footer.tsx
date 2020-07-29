0e9071a08b1a855579921ff375f6795ca7d57bfc
Carolyn Stransky <carolynstransky@gmail.com>
2020-07-29 21:17:52 +0200

    [MEM-1108/1117] Fix a11y issues in social links

--- src/components/organisms/footer.tsx
+++ src/components/organisms/footer.tsx
@@ -17,7 +17,7 @@ export function Footer() {
       <Box maxW="1000px" mx="auto">
         <Flex wrap="wrap" justify="center">
           <Stack my={4} w={["100%", "33%"]}>
-            <Heading as="h4" color="gray.900" fontSize="xl" fontWeight={900}>
+            <Heading as="h3" color="gray.900" fontSize="xl" fontWeight={900}>
               Company
             </Heading>
             <Link
@@ -55,7 +55,7 @@ export function Footer() {
             </Link>
           </Stack>
           <Stack my={4} w={["100%", "33%"]}>
-            <Heading as="h4" color="gray.900" fontSize="xl" fontWeight={900}>
+            <Heading as="h3" color="gray.900" fontSize="xl" fontWeight={900}>
               Resources
             </Heading>
             <Link
@@ -91,7 +91,7 @@ export function Footer() {
             </Link>
           </Stack>
           <Stack my={4} w={["100%", "33%"]}>
-            <Heading as="h4" color="gray.900" fontSize="xl" fontWeight={900}>
+            <Heading as="h3" color="gray.900" fontSize="xl" fontWeight={900}>
               Related links
             </Heading>
             <Link
@@ -124,7 +124,12 @@ export function Footer() {
             </Link>
           </Stack>
         </Flex>
-        <Divider borderColor="gray.300" mb={4} mt={4} />
+        <Divider
+          title="Meeshkan social links"
+          borderColor="gray.300"
+          mb={4}
+          mt={4}
+        />
         <Flex justifyContent="space-between" align="center">
           <GatsbyLink to="/" aria-label="Meeshkan homepage">
             <Icon name="Logo" color="gray.900" size="6" w="auto" />
@@ -135,6 +140,7 @@ export function Footer() {
               variant="ghost"
               isExternal
               href="https://twitter.com/meeshkan"
+              aria-label="Twitter"
               // @ts-ignore
               icon="twitter"
               rounded="sm"
@@ -145,6 +151,7 @@ export function Footer() {
               variant="ghost"
               isExternal
               href="https://www.linkedin.com/company/meeshkan/"
+              aria-label="LinkedIn"
               // @ts-ignore
               icon="linkedin"
               rounded="sm"
@@ -155,6 +162,7 @@ export function Footer() {
               variant="ghost"
               isExternal
               href="https://github.com/meeshkan"
+              aria-label="GitHub"
               // @ts-ignore
               icon="github"
               rounded="sm"
