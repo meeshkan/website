c7f80bf7dcfb710c7e499d9d24fae8a2b3f45f68
Makenna Smutz <makennasmutz@gmail.com>
2020-08-06 17:30:28 +0200

    hotfix error message for mini tester

--- src/pages/test-graphql.tsx
+++ src/pages/test-graphql.tsx
@@ -271,119 +271,126 @@ const TestGraphqlPage = () => {
             <Heading mb={4} fontWeight={900} fontSize="xl">
               Test Results:
             </Heading>
-            {testResults.commands.map((command, index) => (
-              <Accordion defaultIndex={[0]} allowMultiple>
-                {command.exchange.map((exchange, index) => (
-                  <AccordionItem
-                    key={index}
-                    border="none"
-                    mb={8}
-                    borderRadius="sm"
-                    backgroundColor="white"
-                  >
-                    <Box
+            {testResults.commands[0].exchange.length >= 1 ? (
+              testResults.commands.map((command, index) => (
+                <Accordion defaultIndex={[0]} allowMultiple>
+                  {command.exchange.map((exchange, index) => (
+                    <AccordionItem
                       key={index}
                       border="none"
                       mb={8}
                       borderRadius="sm"
                       backgroundColor="white"
                     >
-                      <AccordionHeader
-                        _hover={{
-                          backgroundColor: "none",
-                        }}
-                        _focus={{
-                          outline: "none",
-                          borderBottom: "1px solid",
-                        }}
-                        _active={{
-                          borderBottom: "1px solid",
-                        }}
+                      <Box
+                        key={index}
+                        border="none"
+                        mb={8}
                         borderRadius="sm"
-                        p={4}
-                        justifyContent="space-between"
+                        backgroundColor="white"
                       >
-                        <Flex>
-                          <Text fontWeight={600} mr={2}>
-                            {exchange.meta.apiType === "graphql"
-                              ? JSON.parse(exchange.request.body)[
-                                  "query"
-                                ].startsWith("query")
-                                ? "QUERY"
-                                : JSON.parse(exchange.request.body)[
+                        <AccordionHeader
+                          _hover={{
+                            backgroundColor: "none",
+                          }}
+                          _focus={{
+                            outline: "none",
+                            borderBottom: "1px solid",
+                          }}
+                          _active={{
+                            borderBottom: "1px solid",
+                          }}
+                          borderRadius="sm"
+                          p={4}
+                          justifyContent="space-between"
+                        >
+                          <Flex>
+                            <Text fontWeight={600} mr={2}>
+                              {exchange.meta.apiType === "graphql"
+                                ? JSON.parse(exchange.request.body)[
                                     "query"
-                                  ].startsWith("mutation")
-                                ? "MUTATION"
-                                : JSON.parse(exchange.request.body)[
-                                    "query"
-                                  ].startsWith("subscription")
-                                ? "SUBSCRIPTION"
-                                : exchange.request.method.toUpperCase()
-                              : exchange.request.method.toUpperCase()}
-                          </Text>
-                          <Text fontWeight={600}>{exchange.meta.path}</Text>
-                        </Flex>
-                        <AccordionIcon />
-                      </AccordionHeader>
-                      <AccordionPanel py={4}>
-                        <Flex>
-                          {exchange.response.statusCode && (
-                            <Code
-                              fontWeight={700}
-                              mb={2}
-                              fontSize="md"
-                              variantColor="gray"
-                            >
-                              HTTP status code: {exchange.response.statusCode}
-                            </Code>
+                                  ].startsWith("query")
+                                  ? "QUERY"
+                                  : JSON.parse(exchange.request.body)[
+                                      "query"
+                                    ].startsWith("mutation")
+                                  ? "MUTATION"
+                                  : JSON.parse(exchange.request.body)[
+                                      "query"
+                                    ].startsWith("subscription")
+                                  ? "SUBSCRIPTION"
+                                  : exchange.request.method.toUpperCase()
+                                : exchange.request.method.toUpperCase()}
+                            </Text>
+                            <Text fontWeight={600}>{exchange.meta.path}</Text>
+                          </Flex>
+                          <AccordionIcon />
+                        </AccordionHeader>
+                        <AccordionPanel py={4}>
+                          <Flex>
+                            {exchange.response.statusCode && (
+                              <Code
+                                fontWeight={700}
+                                mb={2}
+                                fontSize="md"
+                                variantColor="gray"
+                              >
+                                HTTP status code: {exchange.response.statusCode}
+                              </Code>
+                            )}
+                          </Flex>
+                          {exchange.request.body && (
+                            <>
+                              <Heading
+                                as="h4"
+                                fontSize="lg"
+                                my={4}
+                                fontWeight={900}
+                              >
+                                Request body:
+                              </Heading>
+                              <CodeBlock className="graphql">
+                                {prettier.format(
+                                  JSON.parse(exchange.request.body)["query"],
+                                  {
+                                    parser: "graphql",
+                                    plugins: [parserGraphql],
+                                  }
+                                )}
+                              </CodeBlock>
+                            </>
                           )}
-                        </Flex>
-                        {exchange.request.body && (
-                          <>
-                            <Heading
-                              as="h4"
-                              fontSize="lg"
-                              my={4}
-                              fontWeight={900}
-                            >
-                              Request body:
-                            </Heading>
-                            <CodeBlock className="graphql">
-                              {prettier.format(
-                                JSON.parse(exchange.request.body)["query"],
-                                {
-                                  parser: "graphql",
-                                  plugins: [parserGraphql],
-                                }
-                              )}
-                            </CodeBlock>
-                          </>
-                        )}
-                        {exchange.response.body && (
-                          <>
-                            <Heading
-                              as="h4"
-                              fontSize="lg"
-                              my={4}
-                              fontWeight={900}
-                            >
-                              Response body:
-                            </Heading>
-                            <CodeBlock className="json">
-                              {JSON.stringify(
-                                JSON.parse(exchange.response.body),
-                                null,
-                                2
-                              )}
-                            </CodeBlock>
-                          </>
-                        )}
-                      </AccordionPanel>
-                    </Box>
-                  </AccordionItem>
-                ))}
-              </Accordion>
-            ))}
+                          {exchange.response.body && (
+                            <>
+                              <Heading
+                                as="h4"
+                                fontSize="lg"
+                                my={4}
+                                fontWeight={900}
+                              >
+                                Response body:
+                              </Heading>
+                              <CodeBlock className="json">
+                                {JSON.stringify(
+                                  JSON.parse(exchange.response.body),
+                                  null,
+                                  2
+                                )}
+                              </CodeBlock>
+                            </>
+                          )}
+                        </AccordionPanel>
+                      </Box>
+                    </AccordionItem>
+                  ))}
+                </Accordion>
+              ))
+            ) : (
+              <Text>
+                Could not build graphql tests. Endpoint schema does not contain
+                any queries, mutations or subscriptions.
+              </Text>
+            )}
           </Box>
         </>
       ) : null}
