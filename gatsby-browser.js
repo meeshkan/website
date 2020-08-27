059a00d76b96e5611f7aeabe72e58a521b93d234
Mike Solomon <mike@meeshkan.com>
2020-08-27 19:16:38 +0300

    Uses haskell

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,7 +4,6 @@ import customTheme from "./theme/theme"
 import Prism from "prism-react-renderer/prism"
 ;(typeof global !== "undefined" ? global : window).Prism = Prism
 require("prismjs/components/prism-haskell")
-require("prismjs/components/prism-purescript")
 require("prismjs/components/prism-scheme")
 require("prismjs/components/prism-latex")
 
