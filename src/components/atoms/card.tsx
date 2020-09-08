dd53655d372fcb446d7973a61f46108e24ed5d2f
Makenna Smutz <makennasmutz@gmail.com>
2020-09-08 22:27:07 +0200

    pricing styling updates

--- src/components/atoms/card.tsx
+++ src/components/atoms/card.tsx
@@ -3,66 +3,71 @@ import { Box, Heading, Text } from "@chakra-ui/core"
 import { UniversalLink } from "./UniversalLink"
 
 type CardProps = {
-  children?: Object
-  heading?: string
-  body?: string
-  link?: string
-  label?: string
-  backgroundColor?: string
+	children?: Object
+	heading?: string
+	body?: string
+	link?: string
+	label?: string
+	backgroundColor?: string
 }
 
 export const Card = ({
-  children,
-  heading,
-  body,
-  link,
-  label,
-  backgroundColor = "white",
+	children,
+	heading,
+	body,
+	link,
+	label,
+	backgroundColor = "white",
 }: CardProps) => {
-  return (
-    <>
-      {link ? (
-        <Box
-          as={UniversalLink}
-          // @ts-ignore
-          to={link}
-          aria-label={label}
-          borderRadius="md"
-          backgroundColor={backgroundColor ? backgroundColor : `gray.50`}
-          p={6}
-          position="relative"
-        >
-          {heading ? (
-            <Heading
-              as="h3"
-              color="gray.900"
-              fontSize="xl"
-              fontWeight={900}
-              mb={4}
-            >
-              {heading}
-            </Heading>
-          ) : null}
-          {body ? <Text mb={6}>{body}</Text> : null}
-          {children}
-        </Box>
-      ) : (
-        <Box borderRadius="sm" backgroundColor="gray.50" p={6}>
-          {heading ? (
-            <Heading
-              as="h3"
-              color="gray.900"
-              fontSize="2xl"
-              fontWeight={900}
-              mb={4}
-            >
-              {heading}
-            </Heading>
-          ) : null}
-          {body ? <Text mb={6}>{body}</Text> : null}
-          {children}
-        </Box>
-      )}
-    </>
-  )
+	return (
+		<>
+			{link ? (
+				<Box
+					as={UniversalLink}
+					// @ts-ignore
+					to={link}
+					aria-label={label}
+					borderRadius="md"
+					backgroundColor={backgroundColor ? backgroundColor : `gray.50`}
+					p={6}
+					position="relative"
+				>
+					{heading ? (
+						<Heading
+							as="h3"
+							color="gray.900"
+							fontSize="xl"
+							fontWeight={900}
+							mb={4}
+						>
+							{heading}
+						</Heading>
+					) : null}
+					{body ? <Text mb={6}>{body}</Text> : null}
+					{children}
+				</Box>
+			) : (
+				<Box
+					borderRadius="sm"
+					backgroundColor="gray.50"
+					p={6}
+					position="relative"
+				>
+					{heading ? (
+						<Heading
+							as="h3"
+							color="gray.900"
+							fontSize="2xl"
+							fontWeight={900}
+							mb={4}
+						>
+							{heading}
+						</Heading>
+					) : null}
+					{body ? <Text mb={6}>{body}</Text> : null}
+					{children}
+				</Box>
+			)}
+		</>
+	)
 }
--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -1,201 +1,191 @@
 import React from "react"
 import {
-  SimpleGrid,
-  Text,
-  Heading,
-  Stack,
-  Flex,
-  Icon,
-  Button,
-  Link as ChakraLink,
-  Code,
+	SimpleGrid,
+	Text,
+	Heading,
+	Stack,
+	Flex,
+	Icon,
+	Button,
+	Code,
+	Box,
 } from "@chakra-ui/core"
 import { Card } from "../components/atoms/card"
 import { useMixpanel } from "gatsby-plugin-mixpanel"
 import { UniversalLink } from "../components/atoms/UniversalLink"
 import { SingleSection } from "../components/organisms/singleSection"
 import Layout from "../components/templates/layout"
-import { Link } from "gatsby"
 import SEO from "../components/molecules/seo"
 
 type PricingProps = {
-  title: string
-  subtitle?: string
-  price: string
-  yesFeatures?: Array<string>
-  noFeatures?: Array<string>
-  hasCTA: boolean
-  mixpanel: any
+	title: string
+	subtitle?: string
+	price: string
+	yesFeatures?: Array<string>
+	noFeatures?: Array<string>
+	hasCTA: boolean
+	mixpanel: any
+	CTA?: string
 }
 
 const PricingCard = ({
-  title,
-  subtitle,
-  price,
-  yesFeatures,
-  noFeatures,
-  hasCTA,
-  mixpanel,
+	title,
+	subtitle,
+	price,
+	yesFeatures,
+	noFeatures,
+	hasCTA,
+	mixpanel,
+	CTA,
 }: PricingProps) => (
-  <Card>
-    <Heading
-      as="h3"
-      color="gray.900"
-      fontSize="2xl"
-      fontWeight={900}
-      mb={4}
-      d="flex"
-      justifyContent="center"
-      alignItems="center"
-    >
-      {title}
-      <Code variantColor="blue" fontSize="md" ml={3}>
-        {subtitle}
-      </Code>
-    </Heading>
-    <Text textAlign="center" fontSize="xl" color="gray.700" fontWeight={600}>
-      {price} <span style={{ color: "#616E7C", fontWeight: 400 }}>/month</span>
-    </Text>
+	<Card>
+		<Heading
+			as="h3"
+			color="gray.900"
+			fontSize="2xl"
+			fontWeight={900}
+			mb={4}
+			d="flex"
+			justifyContent="center"
+			alignItems="center"
+		>
+			{title}
+			<Code variantColor="blue" fontSize="md" ml={3}>
+				{subtitle}
+			</Code>
+		</Heading>
+		<Text textAlign="center" fontSize="xl" color="gray.700" fontWeight={600}>
+			{price} <span style={{ color: "#616E7C", fontWeight: 400 }}>/month</span>
+		</Text>
 
-    <Stack spacing={2} mt={4}>
-      {yesFeatures &&
-        yesFeatures.map((feature, index) => (
-          <Flex key={index} align="center">
-            <Icon name="checkmark" color="cyan.500" mr={3} />
-            <Text>{feature}</Text>
-          </Flex>
-        ))}
+		<Stack spacing={2} mt={4}>
+			{yesFeatures &&
+				yesFeatures.map((feature, index) => (
+					<Flex key={index} align="top">
+						<Icon name="checkmark" color="cyan.500" mr={3} mt={2} />
+						<Text>{feature}</Text>
+					</Flex>
+				))}
 
-      {noFeatures &&
-        noFeatures.map((feature, index) => (
-          <Flex key={index} align="center">
-            <Icon name="xmark" color="red.500" mr={3} />
-            <Text>{feature}</Text>
-          </Flex>
-        ))}
-    </Stack>
+			{noFeatures &&
+				noFeatures.map((feature, index) => (
+					<Flex key={index} align="center">
+						<Icon name="xmark" color="red.500" mr={3} />
+						<Text>{feature}</Text>
+					</Flex>
+				))}
+		</Stack>
 
-    {hasCTA && (
-      <Button
-        as={UniversalLink}
-        href="https://app.meeshkan.com"
-        aria-label="Create a free account on Meeshkan."
-        variantColor="red"
-        borderRadius="sm"
-        fontWeight={700}
-        w="full"
-        mt={8}
-        onClick={() => {
-          mixpanel.track("Clicked a button", {
-            to: "https://app.meeshkan.com",
-            from: "https://meeshkan.com/pricing",
-            c2a: `Create an account - ${title}`,
-          })
-        }}
-      >
-        Create a free account
-      </Button>
-    )}
-  </Card>
+		{hasCTA && (
+			<Box mt={8}>
+				<Flex pos="absolute" bottom={4} right={6} left={6}>
+					<Button
+						as={UniversalLink}
+						// @ts-expect-error
+						href="https://app.meeshkan.com"
+						aria-label="Create a free account on Meeshkan."
+						variantColor="red"
+						variant={CTA === `Sign up for Pro` ? "solid" : "outline"}
+						borderRadius="sm"
+						fontWeight={700}
+						w="full"
+						onClick={() => {
+							mixpanel.track(`Create an account - ${title}`, {
+								to: "https://app.meeshkan.com",
+								from: "https://meeshkan.com/pricing",
+								c2a: `Create an account - ${title}`,
+							})
+						}}
+					>
+						{CTA}
+					</Button>
+				</Flex>
+			</Box>
+		)}
+	</Card>
 )
 
 const PricingPage = () => {
-  const mixpanel = useMixpanel()
-  return (
-    <Layout>
-      <SEO
-        pageTitle="Pricing"
-        pageDescription="Pricing tiers for using the automated testing tool, Meeshkan."
-        pageUrl="https://meeshkan.com/pricing/"
-      />
-      <SingleSection>
-        <Heading
-          as="h1"
-          color="gray.900"
-          fontSize="3xl"
-          fontWeight={900}
-          textAlign="center"
-          mb={6}
-          letterSpacing="wide"
-          lineHeight="short"
-        >
-          Meeshkan Pricing
-        </Heading>
-        <Text fontSize="2xl" textAlign="center" mb={12} lineHeight="tall">
-          Meeshkan is the equivialent of 1, 60k+ salaried QA engineer, for every
-          5 engineers hired. While we are still in private beta, Meeshkan is
-          free for fair use.
-          <ChakraLink
-            // @ts-ignore
-            as={Link}
-            color="blue.500"
-          >
-            {" "}
-            Let us know
-          </ChakraLink>{" "}
-          if we can build a plan for you.
-        </Text>
-        <SimpleGrid columns={3} spacing={8}>
-          <PricingCard
-            title="Free"
-            subtitle="for Individuals"
-            price="$0"
-            mixpanel={mixpanel}
-            yesFeatures={[
-              "1 team member",
-              "2 projects",
-              "Manual project setup",
-              "100 testing hours",
-              "GitHub import",
-            ]}
-            // noFeatures={[
-            //   "Concurrent tests",
-            //   "GitLab & Bitbucket",
-            //   "Test history",
-            //   "Audit reports",
-            // ]}
-            hasCTA={true}
-          />
-          <PricingCard
-            title="Pro"
-            subtitle="for Teams"
-            price="$99"
-            mixpanel={mixpanel}
-            yesFeatures={[
-              "8 team members",
-              "Unlimited projects",
-              "1 project set up",
-              "1000 testing hours",
-              "3 concurrent tests",
-              "Weekly audit reporting",
-              "30 day history",
-            ]}
-            hasCTA={false}
-          />
-          <PricingCard
-            title="Business"
-            subtitle="starting at"
-            mixpanel={mixpanel}
-            price="$2000"
-            yesFeatures={[
-              "25 team members",
-              "Unlimited projects",
-              "5 projects set up",
-              "Unlimited testing hours",
-              "10 concurrent tests",
-              "Unlimited history",
-              "GitLab & Bitbucket import",
-              "Auth flow testing UI",
-              "Custom build pipelines",
-              "Role based permissions",
-              "Jira/Linear integration",
-            ]}
-            hasCTA={false}
-          />
-        </SimpleGrid>
-      </SingleSection>
-    </Layout>
-  )
+	const mixpanel = useMixpanel()
+	return (
+		<Layout>
+			<SEO
+				pageTitle="Pricing"
+				pageDescription="Pricing tiers for using the automated testing tool, Meeshkan."
+				pageUrl="https://meeshkan.com/pricing/"
+			/>
+			<SingleSection>
+				<Heading
+					as="h1"
+					color="gray.900"
+					fontSize="3xl"
+					fontWeight={900}
+					textAlign="center"
+					mb={6}
+					letterSpacing="wide"
+					lineHeight="short"
+				>
+					Meeshkan Pricing
+				</Heading>
+				<Text fontSize="2xl" textAlign="center" mb={12} lineHeight="tall">
+					Quality assurance isn't all-or-nothing. Our pricing scales with your
+					team's need, and we have a generous free tier that integrates
+					seamlessly into your software engineer's day-to-day flow.
+				</Text>
+				<SimpleGrid columns={3} spacing={8}>
+					<PricingCard
+						title="Free"
+						subtitle="for Individuals"
+						CTA="Create a free account"
+						price="$0"
+						mixpanel={mixpanel}
+						yesFeatures={[
+							"Endpoint testing for REST and GraphQL services",
+							"Basic reports",
+							"100 testing hours",
+							"GitHub integration",
+						]}
+						hasCTA={true}
+					/>
+					<PricingCard
+						title="Pro"
+						subtitle="for Teams"
+						price="$199"
+						mixpanel={mixpanel}
+						yesFeatures={[
+							"All free features",
+							"Unlimited projects",
+							"1000 testing hours",
+							"3 concurrent tests",
+							"Premium reports",
+							"30 day history retention",
+						]}
+						hasCTA={true}
+						CTA="Sign up for Pro"
+					/>
+					<PricingCard
+						title="Business"
+						subtitle="starting at"
+						hasCTA={true}
+						CTA="Sign up for Business"
+						mixpanel={mixpanel}
+						price="$2499"
+						yesFeatures={[
+							"All Pro features",
+							"Unlimited projects",
+							"Unlimited testing hours",
+							"Unlimited history",
+							"GitLab & Bitbucket import",
+							"Custom build pipelines",
+							"In-cluster testing for k8s",
+							"Role based permissions",
+							"Jira/Linear integration",
+						]}
+					/>
+				</SimpleGrid>
+			</SingleSection>
+		</Layout>
+	)
 }
 
 export default PricingPage
