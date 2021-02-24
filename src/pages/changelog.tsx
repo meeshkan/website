3299531403d64681d252f6fdb4d92069d355c212
Makenna Smutz <makennasmutz@gmail.com>
2021-02-24 18:57:20 +0100

    Add base

--- src/pages/changelog.tsx
+++ src/pages/changelog.tsx
@@ -1,11 +1,19 @@
 import React from "react"
-import { Box, Heading } from "@chakra-ui/react"
+import {} from "@chakra-ui/react"
+import Layout from "../components/templates/layout"
+import SEO from "../components/molecules/seo"
+import { SingleSection } from "../components/organisms/singleSection"
 
 const ChangelogPage = () => {
 	return (
-		<Box>
-			<Heading>Hey</Heading>
-		</Box>
+		<Layout>
+			<SEO
+				pageTitle="Meeshkan Changelog"
+				pageDescription="The Meeshkan changelog communicating what we've accomplished as a team."
+				pageUrl="https://meeshkan.com/changelog/"
+			/>
+			<SingleSection>yo</SingleSection>
+		</Layout>
 	)
 }
 
