3e8834e67e2b2557cce33f86b33c96f77f64332b
k4m4 <nikolaskam@gmail.com>
2021-05-20 13:46:17 +0300

    Create PH SVG icons

--- src/components/molecules/product-hunt-badge.tsx
+++ src/components/molecules/product-hunt-badge.tsx
@@ -4,6 +4,10 @@ import {
 	Image,
 	useColorMode,
 } from '@chakra-ui/react'
+import {
+	ProductHuntDarkBadge,
+	ProductHuntLightBadge,
+} from '../../../theme/icons'
 
 const ProductHuntBadge = () => {
 	const { colorMode } = useColorMode()
@@ -13,14 +17,14 @@ const ProductHuntBadge = () => {
 			isExternal
 			textDecoration="none"
 			_hover={{ textDecoration: 'none' }}
+			aria-label="Meeshkan UI Test Recorder - No-code test writing that speeds up your release cycle | Product Hunt"
 			textAlign="center"
 		>
-			<Image
-				src={`https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=296495&theme=${colorMode}`}
-				alt="Meeshkan UI Test Recorder - No-code test writing that speeds up your release cycle | Product Hunt"
-				width="250px"
-				height="54px"
-			/>
+			{colorMode === 'light' ? (
+				<ProductHuntLightBadge />
+			) : (
+				<ProductHuntDarkBadge />
+			)}
 		</Link>
 	)
 }
