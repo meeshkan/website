009e79a77b8ca4b33d168a44f72e820ca39bedf5
k4m4 <nikolaskam@gmail.com>
2021-05-20 13:04:19 +0300

    Add PH badge to landing page

---
+++ src/components/molecules/product-hunt-badge.tsx
@@ -0,0 +1,26 @@
+import {
+	Link,
+	Image,
+	useColorMode,
+} from '@chakra-ui/react';
+
+const ProductHuntBadge = () => {
+	const { colorMode } = useColorMode();
+	return (
+		<Link
+			href="https://www.producthunt.com/posts/meeshkan-ui-test-recorder?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-meeshkan-ui-test-recorder"
+			isExternal
+			textDecoration="none"
+			_hover={{ textDecoration: 'none' }}
+		>
+			<Image
+				src={`https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=296495&theme=${colorMode}`}
+				alt="Meeshkan UI Test Recorder - No-code test writing that speeds up your release cycle | Product Hunt"
+				width="250px"
+				height="54px"
+			/>
+		</Link>
+	);
+};
+
+export default ProductHuntBadge;
